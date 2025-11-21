77536b625aec57a4bd562f3c56d0764de778eb42
Joseph Wang <joseph@harmonic.ai>
2025-11-20 17:38:18 -0800

    Update version in handler.lua to 2.0.0

--- kong/plugins/remote-jwt-auth/handler.lua
+++ kong/plugins/remote-jwt-auth/handler.lua
@@ -9,7 +9,7 @@ local jwt_decoder = require("kong.plugins.jwt.jwt_parser")
 local assert = assert
 
 local PubSubHandler = {
-    VERSION = "1.0.0",
+    VERSION = "2.0.0",
     PRIORITY = 1500,
 }
 
